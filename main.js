4b26dca6c76113ca0099bbb7112fe7b4bcf534d1
KingGamzz <84605137+KingGamzz@users.noreply.github.com>
2021-10-11 17:59:05 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+Webcam.set({
+    height: 350,
+    width: 350,
+    image_format: 'jpeg',
+    jpeg_quality: 100
+});
+
+camera = document.getElementById("camera");
+Webcam.attach('#camera');
+
+
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="+data_uri+">'
+    });
+}
+console.log('ml5 verion:', ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/3t7By2UtQ/', model loaded);
+function modelloaded(){
+    console.log('modelloaded');
+}
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+    }
+}
